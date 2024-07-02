b13242348bab7aa392c9dfbcc7b141dd253f156d
Matheus <szmatheussouza@gmail.com>
2024-07-01 23:55:01 -0300

    inserido primeiro arquivo.php

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -8,19 +8,12 @@ class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-        return redirect('https://www.google.com/');
         $series = [
             'Peaky Blinders',
             'Breaking Bad',
             'La casa de papel',
         ];
 
-        $html = '<ul>';
-        foreach ($series as $serie){
-            $html .= "<li>$serie</li>";
-        }
-        $html .= '</ul>';
-
-        return $html;
+    return view('listar-series', compact('series'));
     }
 }
