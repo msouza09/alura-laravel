c1380448d315e099b5eaa1ac141324a226c9fd2f
Matheus <szmatheussouza@gmail.com>
2024-07-13 12:09:32 -0300

    inicio plataforma

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/series', [SeriesController::class, 'index'])->name('series.index');
-
-Route::get('/series/criar', [SeriesController::class, 'create'])->name('series.create');
-
-Route::post('/series/salvar', [SeriesController::class, 'store'])->name('series.store');
-
-Route::delete('series/delete/{id}', [SeriesController::class, 'destroy'])->name('series.destroy');
