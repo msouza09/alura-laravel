a7af638ff1a240e283f9d2ea8a39e8c59104290c
Matheus <szmatheussouza@gmail.com>
2024-07-20 17:46:36 -0300

    teste

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -4,7 +4,15 @@
         <div class="mb-3" >
         <label for="nome" class="form-label">Nome:</label>
         <input type="text" id="nome" name="nome" class="form-control">
+        <label for="nome" class="form-label">Gênero:</label>
+        <input type="text" id="nome" name="nome" class="form-control">
+        <label for="nome" class="form-label">Ano de Lançamento:</label>
+        <input type="text" id="nome" name="nome" class="form-control">
+        <label for="nome" class="form-label">Número de Temporadas:</label>
+        <input type="text" id="nome" name="nome" class="form-control">
+        <label for="nome" class="form-label">Descrição:</label>
+        <input type="text" id="nome" name="nome" class="form-control">
         </div>
-        <button type="submit" class="btn btn-dark">Adicionar</button>
+        <button type="submit" class="btn btn-dark">Adicionar Série</button>
     </form>
 </x-layout>
